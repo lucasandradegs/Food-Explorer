54ecad59279928132a37248037e563bf692b9517
Lucas Andrade <lucas.goncalvessousa@aperam.com>
2023-10-11 11:49:25 -0300

    update user password

--- Backend/src/controller/UsersController.js
+++ Backend/src/controller/UsersController.js
@@ -1,5 +1,5 @@
 const knex = require("../database/knex")
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 class UsersController {
@@ -16,11 +16,11 @@ class UsersController {
 
         await knex("users").insert({ name, email, password: hashedPassword })
 
-        return res.status(201).json({ name, email, password })
+        return res.status(201).json(`Usuário cadastrado com sucesso!`)
     }
 
     async update(req, res) {
-        const { name, email } = req.body
+        const { name, email, password, old_password } = req.body
         const { id } = req.params
 
         const user = await knex("users").where({ id }).first()
@@ -31,14 +31,28 @@ class UsersController {
 
         const userWithUpdatedEmail = await knex("users").where({ email }).first()
     
-        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== id ) {
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id ) {
             throw new AppError(`Endereço de e-mail já está em uso.`)
         }
 
         user.name = name ?? user.name
         user.email = email ?? user.email
 
-        await knex("users").update({ name, email, updated_at: knex.fn.now() }).where({ id })
+        if (password && !old_password) {
+            throw new AppError(`Você precisa informar a senha antiga para definir a nova senha`)
+        }
+        
+        if (password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password)
+        
+            if (!checkOldPassword) {
+                throw new AppError(`A senha antiga não confere!`)
+            }
+
+            user.password = await hash(password, 8)
+        }
+
+        await knex("users").update({ name, email, password: user.password, updated_at: knex.fn.now() }).where({ id })
     
         return res.status(201).json(`Usuário atualizado com sucesso!`)
     }
